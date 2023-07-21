41090db3fe2085f456d04b275d6ce8d34bcb7046
Martin Lokvenc01~ <lokvenc.martin@gmail.com>
2023-07-21 16:39:45 +0200

    Initial commit

---
+++ tracker.py
@@ -0,0 +1,26 @@
+
+    
+class Contacts():
+    def __init__(input_path):
+
+    def load(input_path):
+
+    def save(output_path):
+
+
+class Person():
+    def __init__(self, first_name, middle_name="", last_name=""):
+
+
+
+
+class Greeting():
+
+
+def main():
+
+
+
+
+if __name__ == "__main__":
+    main()
